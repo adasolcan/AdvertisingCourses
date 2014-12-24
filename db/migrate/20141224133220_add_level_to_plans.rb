4b948c9d67194b8321804c5d74394480734dab48
adasolcan <adasolcan@yahoo.com>
2014-12-24 18:10:50 +0200

    added level to users

---
+++ db/migrate/20141224133220_add_level_to_plans.rb
@@ -0,0 +1,5 @@
+class AddLevelToPlans < ActiveRecord::Migration
+  def change
+    add_column :users, :level, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223225541) do
+ActiveRecord::Schema.define(version: 20141224133220) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141223225541) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.integer  "level"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
